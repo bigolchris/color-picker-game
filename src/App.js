855f2fdfaadf6d1ee27b39c51774cd49c601f677
Christian Sanchez <52680827+bigolchris@users.noreply.github.com>
2022-12-27 21:04:04 -0800

    Everything is displaying
    -color picker fully functioning

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@ import React, { useState } from "react";
 
 import "./index.css";
 
-const generateHex = (size) => {
+const generateHex = (size) =>
   [...Array(size)]
     .map(() => Math.floor(Math.random() * 16).toString(16))
     .join("");
-};
 
 export default function App() {
   const [score, setScore] = useState(0);
