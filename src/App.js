f64df0016aea090b8a8a858d593e08c9a922dfe8
Christian Sanchez <52680827+bigolchris@users.noreply.github.com>
2022-12-27 20:49:23 -0800

    added functionality
    -added functions for game to work
    -not working properlt

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 
+import "./index.css";
+
 const generateHex = (size) => {
   [...Array(size)]
     .map(() => Math.floor(Math.random() * 16).toString(16))
@@ -25,4 +27,67 @@ export default function App() {
     }
     setNext(true);
   };
+
+  const shuffleColors = () => {
+    let newColor = "#" + generateHex(6);
+    setColor(newColor);
+    setArray(shuffle(["#" + generateHex(6), "#" + generateHex(6), newColor]));
+  };
+
+  function shuffle(array) {
+    let currentIndex = array.length,
+      randomIndex;
+
+    while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ];
+    }
+
+    return array;
+  }
+
+  return (
+    <div
+      className="color-picker"
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        gap: "1em",
+      }}
+    >
+      <div>Score: {score}</div>
+      <div
+        style={{ width: "250px", height: "250px", backgroundColor: color }}
+      ></div>
+      <div style={{ display: "flex", gap: "2em" }}>
+        {array.map((item, i) => (
+          <button
+            onClick={() => checkColor(item)}
+            key={i}
+            style={next ? { background: item } : { background: "white" }}
+          >
+            <p>{item}</p>
+          </button>
+        ))}
+      </div>
+      {next && (
+        <button
+          onClick={() => {
+            shuffleColors();
+            setMessage("");
+            setNext(false);
+          }}
+        >
+          Next
+        </button>
+      )}
+      <div>{message}</div>
+    </div>
+  );
 }
